8c0ad4f424ccf3539b0130eb40c81834498fc3c8
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-03-08 17:07:22 +0530

    Added Header and landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const imageUrl = "https://avatars.githubusercontent.com/u/86121130?v=4";
+const greeting = "Hello, I am Ansul Agrawal!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-
+  <FullScreenSection justifyContent="center" alignItems="center" isDarkBackground backgroundColor="#2A4365">
+    <Avatar size="2xl" name="Ansul Agrawal" src={imageUrl} />
+    <Text fontSize="2xl">{greeting}</Text>
+    <Text fontSize="6xl" as="b">
+      {bio1}
+    </Text>
+    <Text fontSize="6xl" as="b">
+      {bio2}
+    </Text>
   </FullScreenSection>
 );
 
